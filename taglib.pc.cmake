dcf4e27fe827b9bf21a1e896378597391ee5f251
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-09-12 18:14:11 +0200

    taglib.pc: require stlport for android

--- taglib.pc.cmake
+++ taglib.pc.cmake
@@ -5,7 +5,7 @@ includedir=${INCLUDE_INSTALL_DIR}
 
 Name: TagLib
 Description: Audio meta-data library
-Requires: 
+Requires:  ${STLPORT_PC}
 Version: ${TAGLIB_LIB_MAJOR_VERSION}.${TAGLIB_LIB_MINOR_VERSION}.${TAGLIB_LIB_PATCH_VERSION}
 Libs: -L${LIB_INSTALL_DIR} -ltag
 Cflags: -I${INCLUDE_INSTALL_DIR}/taglib 
